a16a42d0d183863f997026625999acf6d46a52bf
nicetech-creator <toptech20190609@gmail.com>
2021-09-07 00:51:36 +0800

    process template

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -2,6 +2,8 @@ use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
+use crate::process::Processor;
+
 entrypoint!(process_instruction);
 
 fn process_instruction(
@@ -9,11 +11,5 @@ fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
+    Processor::process(program_id, accounts, instruction_data)
 }
--- src/process.rs
+++ src/process.rs
@@ -1,10 +1,32 @@
 use solana_program::{
-  account_info::AccountInfo,
+  account_info::{next_account_info, AccountInfo},
   entrypoint::ProgramResult,
   msg,
+  program::{invoke, invoke_signed},
+  program_error::ProgramError,
+  program_pack::{IsInitialized, Pack},
   pubkey::Pubkey,
+  sysvar::{rent::Rent, Sysvar},
 };
 
+use crate::{error::WPOError, instruction::WPOInstruction, state::WPO};
 
 
 pub struct Processor;
+
+impl Processor {
+    pub fn process(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        let instruction = WPOInstruction::unpack(instruction_data)?;
+
+        match instruction {
+            WPOInstruction::Purchase {amount} => {
+                msg!("Instruction: Purchase");
+                Ok(())
+            }
+        }
+    }
+}
