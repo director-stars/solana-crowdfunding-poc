55ada3f6a3b02584888e4d0a523bdea912175251
nicetech-creator <toptech20190609@gmail.com>
2021-09-06 11:18:56 +0800

    Wallet component

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import {Wallet} from './components/wallet';
 import logo from './logo.svg';
 import './App.css';
 
@@ -5,10 +6,10 @@ function App() {
   return (
     <div className="App">
       <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-      <h5 className="my-0 mr-md-auto font-weight-normal">WEBDEV_PHASE1</h5>
-      <nav className="my-2 my-md-0 mr-md-3">
-      </nav>
-      <a className="btn btn-outline-primary" href="#">Connect</a>
+        <h5 className="my-0 mr-md-auto font-weight-normal">WEBDEV_PHASE1</h5>
+        <nav className="my-2 my-md-0 mr-md-3">
+        </nav>
+        <Wallet/>
       </div>
 
       <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
