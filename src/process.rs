dd17bdb572f8377965e073e378ffd7de746c6aa7
nicetech-creator <toptech20190609@gmail.com>
2021-09-07 02:50:54 +0800

    fix: accounts info

--- src/process.rs
+++ src/process.rs
@@ -43,16 +43,22 @@ impl Processor {
             return Err(ProgramError::MissingRequiredSignature);
         }
 
-        /// get PDA for gathering funds
+        let pda_account_info = next_account_info(account_info_iter)?;
         let (pda, _bump_seed) = Pubkey::find_program_address(&[b"lootbox"], program_id);
+        if pda != *pda_account_info.key {
+            msg!("Error: Associated address does not match seed derivation");
+            return Err(ProgramError::InvalidSeeds);
+        }
+
+        let system_program_info = next_account_info(account_info_iter)?;
 
         /// move SOL from user wallet to the lootbox wallet
         invoke(
             &system_instruction::transfer(initializer.key, pda.key, 2000000000),
             &[
                 initializer.clone(),
-                pda.clone(),
-                system_program.clone(),
+                pda_account_info.clone(),
+                system_program_info.clone(),
             ],
         )?;
 
