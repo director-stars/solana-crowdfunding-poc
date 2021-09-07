54dac330f685fc1c98614b2ae20546ce0b63b6f0
nicetech-creator <toptech20190609@gmail.com>
2021-09-07 10:17:32 +0800

    update state to include admin wallet address

--- src/state.rs
+++ src/state.rs
@@ -6,9 +6,18 @@ use solana_program::{
     pubkey::Pubkey,
 };
 
+
+
 /// Define the type of state stored in accounts
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct WPO {
     pub is_initialized: bool,
-    pub supply: u8
+    pub supply: u8,
+    pub admin_wallet: Pubkey,
+}
+
+impl IsInitialized for WPO {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
 }
